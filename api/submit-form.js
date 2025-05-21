82a3ef9456f03b3162fad6e7b732230c3635d5da
jxmuntaner <jxmuntaner@icloud.com>
2025-05-21 03:49:31 +0200

    Revert "cb"
    This reverts commit f9b9e78d6d37638d3f74710bf8f8ed50af91fde6.

--- api/submit-form.js
+++ api/submit-form.js
@@ -61,6 +61,3 @@ export default async function handler(req, res) {
         return res.status(500).json({ message: 'Error sending message. Please try again later.' });
     }
 }
-
-git branch -D main
-git branch -m main
